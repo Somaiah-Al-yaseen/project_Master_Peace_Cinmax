55814730cd24b57531d95d99a698dea7fba0b013
somaiah <somiayaseen59@gmail.com>
2023-12-04 01:24:21 +0300

    migrat

--- Cinmax/database/migrations/2023_12_03_210302_create_users_table.php
+++ Cinmax/database/migrations/2023_12_03_210302_create_users_table.php
@@ -13,12 +13,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->integer('mobile');
             $table->string('password');
-            $table->rememberToken();
+            $table->bigInteger('is_admin');
+            $table->string('user_img');
+            $table->bigInteger('is_delete');
+            $table->integer('age');
             $table->timestamps();
+
         });
     }
 
